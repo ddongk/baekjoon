b197198323b2b5e2683fe0b3f3f7116c2cb42739
ddongk <121601676+ddongk@users.noreply.github.com>
2023-04-03 15:22:17 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -6,14 +6,11 @@ import java.util.StringTokenizer;
 public class Main {
  
 	public static void main(String[] args) throws IOException {
-        
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
- 		String str = br.readLine();
-		StringTokenizer st = new StringTokenizer(str," ");
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		int a = Integer.parseInt(st.nextToken());
 		int b = Integer.parseInt(st.nextToken());
 		
-		System.out.println(a+b);
+		System.out.println(a+b); 
 	}
 }
